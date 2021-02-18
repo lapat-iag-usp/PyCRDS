990e78991fa49f8cf73d1824c435b37c341f3148
Marcia Marques <marcia.marques@alumni.usp.br>
2021-02-18 16:48:16 -0300

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
-sys.path.insert(0, os.path.abspath('../../src/pycrds'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
