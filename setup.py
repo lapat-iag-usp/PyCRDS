27945707ee2b43aaa0f8781f1d0631df776a1aec
Marcia Marques <marcia.marques@alumni.usp.br>
2021-08-09 20:23:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,33 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import pathlib
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='pycrds',
     version='0.0.1',
-    author='Marcia Marques, Daniel Castelo',
-    author_email='marcia.marques@alumni.usp.br',
     description='A library to CRDS (cavity ring-down spectroscopy) data processing',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/marcia-marques/PyCRDS',
-    py_modules=["datafile", "flags", "graphs"],
-    package_dir={'': 'src'},
-    install_requires=[
-        "numpy >= 1.19",
-        "pandas >= 1.1",
-        "bokeh >= 2.2",
-        "matplotlib >= 3.2",
-        "tqdm >= 4.48",
-    ],
+    author='Marcia Marques, Daniel Castelo',
+    author_email='marcia.marques@alumni.usp.br',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Atmospheric Science',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.7',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        "numpy >= 1.19",
+        "pandas >= 1.1",
+        "bokeh >= 2.2",
+        "matplotlib >= 3.2",
+        "tqdm >= 4.48",
     ],
 )
